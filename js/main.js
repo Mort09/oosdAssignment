2c330b770935957e3d46f8eb8a9d58bc8f0f0523
ibro <kabaltech@gmail.com>
2019-01-27 22:34:04 -0700

    update: Last bits of CSS before my brain shuts down for the day

--- js/main.js
+++ js/main.js
@@ -1,2 +1,26 @@
-// navbar items
+// let navCont, navItems, navArrays;
 
+// navCont = document.getElementById('navbar-cont')
+// navItems = navCont.getElementsByClassName('nav-item')
+
+// navArrays = Array.from(navItems);
+
+// navArrays.forEach((navArray) => {
+// for(let i=0; i < navItems.length; i++) {
+//     navItems[i].addEventListener('click', function (e){
+
+//         let current = document.getElementsByClassName("active");
+
+//         current[0].className = current[0].className.replace(" active", "");
+        
+//         this.className += " active";
+//     })
+// }
+// })
+
+// $(function(){
+//     $("nav.navbar ul li").click(function(){
+//         $("nav.navbar ul li").removeClass("active");
+//         $(this).addClass("active");
+//     });
+// });
